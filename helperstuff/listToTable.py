bf353e5bf504209ec03cf5368f46fc91b6847c0a
Kale Stahl <kalestahl@gmail.com>
2025-12-08 22:03:31 -0500

    more stuff

--- helperstuff/listToTable.py
+++ helperstuff/listToTable.py
@@ -8,28 +8,39 @@ def listToTable(filename):
     except Exception as e:
         print(f"An error occurred: {e}")
         return
-    tableString =   "| Pokemon | Trainer | Energy | \n |---|---|---| \n"
+    tableString =   ""
     list = list.splitlines()
     cutIndex1 = [i for i, s in enumerate(list) if "Trainer: " in s][0]
     cutIndex2 = [i for i, s in enumerate(list) if "Energy: " in s][0]
     pokemonList = list[1: cutIndex1-1]
     trainerList = list[cutIndex1 + 1: cutIndex2-1]
     energyList = list[cutIndex2 + 1: ]
-    maxLength = max(len(pokemonList), len(trainerList), len(energyList))
-    for i in range(maxLength):
+    pMaxLen = len(max(pokemonList, key=len))
+    tMaxLen = len(cutOff(max(trainerList, key=len)))
+    eMaxLen = len(cutOff(max(energyList, key=len)))
+    maxRows = max(len(pokemonList), len(trainerList), len(energyList))
+    tableString += f"| {"Pokemon".ljust(pMaxLen)} | {"Trainer".ljust(tMaxLen)} | {"Energy".ljust(eMaxLen)} | \n"
+    tableString += f"|{'-'*(pMaxLen +2)}|{'-'*(tMaxLen +2)}|{'-'*(eMaxLen +2)}| \n"
+    for i in range(maxRows):
         pokemon = pokemonList[i] if i < len(pokemonList) else ""
+        pFiller = pMaxLen - len(pokemon)
+        pokemon += " "* pFiller
         trainer = cutOff(trainerList[i]) if i < len(trainerList) else ""
+        tFiller = tMaxLen - len(trainer)
+        trainer += " "* tFiller
         energy = cutOff(energyList[i]) if i < len(energyList) else ""
+        eFiller = eMaxLen - len(energy)
+        energy += " "* eFiller
         tableString += f"| {pokemon} | {trainer} | {energy} | \n"
     return tableString
 
 def cutOff(input_string):
-    words = input_string.split()  # Split the string into a list of words
+    words = input_string.split() 
     if len(words) >= 2:
-        return " ".join(words[:-2])  # Join all words except the last two
+        return " ".join(words[:-2]) 
     else:
-        return ""  # If there are fewer than two words, return an empty string
+        return ""
 
 
-print(listToTable("\listFile.txt"))
+print(listToTable("E:\_repos\KaleStahl.github.io\helperstuff\listFile.txt"))
 
